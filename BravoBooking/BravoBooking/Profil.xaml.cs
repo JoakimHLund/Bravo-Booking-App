47a630785611e28d356a964f7713227f46c68f0e
Synne Storhaug <s162652@win.dtu.dk>
2017-04-27 14:54:53 +0200

    Lagt til Profil tab page og endra FilesPage= RomTid

---
+++ BravoBooking/BravoBooking/Profil.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ADALForForms.Model;
+using BravoBooking;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BravoBooking
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profil : ContentPage
+    {
+        public Profil()
+        {
+            InitializeComponent();
+            Appearing += ProfilAppearing;
+        }
+
+        private async void ProfilAppearing(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", App.AuthenticationResult.AccessToken);
+            var meData = await client.GetStringAsync("https://graph.microsoft.com/beta/me");
+            var userData = JsonConvert.DeserializeObject<UserModel>(meData);
+            //this.DisplayName.Text = userData.DisplayName;
+        }
+    }
+}
